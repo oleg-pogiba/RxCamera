719c829ac859d87898e6f54f24a6cfbe3c41baf2
Oleg Pogiba <workposst@gmail.com>
2017-09-04 18:44:57 +0300

    Added method in order to set arbitrary FLASH_MODE.

--- lib/src/main/java/com/ragnarok/rxcamera/action/RxCameraActionBuilder.java
+++ lib/src/main/java/com/ragnarok/rxcamera/action/RxCameraActionBuilder.java
@@ -19,149 +19,178 @@ import io.reactivex.ObservableOnSubscribe;
  */
 public class RxCameraActionBuilder {
 
-    private RxCamera rxCamera;
+  private RxCamera rxCamera;
 
-    public RxCameraActionBuilder(RxCamera rxCamera) {
-        this.rxCamera = rxCamera;
-    }
+  public RxCameraActionBuilder(RxCamera rxCamera) {
+    this.rxCamera = rxCamera;
+  }
 
-    /**
-     * set the zoom level of the camera
-     * @param level
-     * @return
-     */
-    public Observable<RxCamera> zoom(final int level) {
-        return Observable.create(new ObservableOnSubscribe<RxCamera>() {
-            @Override
-            public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
-                Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
-                if (!parameters.isZoomSupported()) {
-                    subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_NOT_SUPPORT));
-                    return;
-                }
-                int maxZoomLevel = parameters.getMaxZoom();
-                if (level < 0 || level > maxZoomLevel) {
-                    subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_RANGE_ERROR));
-                    return;
-                }
-                parameters.setZoom(level);
-                rxCamera.getNativeCamera().setParameters(parameters);
-                subscriber.onNext(rxCamera);
-            }
-        });
-    }
-
-    /**
-     * smooth zoom the camera, which will gradually change the preview content
-     * @param level
-     * @return
-     */
-    public Observable<RxCamera> smoothZoom(final int level) {
-        return Observable.create(new ObservableOnSubscribe<RxCamera>() {
-            @Override
-            public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
-                Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
-                if (!parameters.isZoomSupported() || !parameters.isSmoothZoomSupported()) {
-                    subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_NOT_SUPPORT));
-                    return;
-                }
-                int maxZoomLevel = parameters.getMaxZoom();
-                if (level < 0 || level > maxZoomLevel) {
-                    subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_RANGE_ERROR));
-                    return;
-                }
-                rxCamera.getNativeCamera().startSmoothZoom(level);
-                subscriber.onNext(rxCamera);
-            }
-        });
-    }
-
-    public Observable<RxCamera> flashAction(final boolean isOn) {
-        return Observable.create(new ObservableOnSubscribe<RxCamera>() {
-            @Override
-            public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
-                Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
-                if (parameters.getSupportedFlashModes() == null || parameters.getSupportedFlashModes().size() <= 0) {
-                    subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
-                    return;
-                }
-                if (isOn) {
-                    if (parameters.getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_AUTO)) {
-                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
-                        rxCamera.getNativeCamera().setParameters(parameters);
-                        subscriber.onNext(rxCamera);
-                        return;
-                    } else if (parameters.getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_TORCH)) {
-                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                        rxCamera.getNativeCamera().setParameters(parameters);
-                        subscriber.onNext(rxCamera);
-                        return;
-                    } else {
-                        subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
-                    }
-                } else {
-                    if (parameters.getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_OFF)) {
-                        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                        rxCamera.getNativeCamera().setParameters(parameters);
-                        subscriber.onNext(rxCamera);
-                    } else {
-                        subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
-                    }
-                }
-            }
-        });
-    }
-
-    public Observable<RxCamera> areaFocusAction(final List<Camera.Area> focusAreaList) {
-        if (focusAreaList == null || focusAreaList.size() == 0) {
-            return null;
+  /**
+   * set the zoom level of the camera
+   *
+   * @param level
+   * @return
+   */
+  public Observable<RxCamera> zoom(final int level) {
+    return Observable.create(new ObservableOnSubscribe<RxCamera>() {
+      @Override
+      public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
+        Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
+        if (!parameters.isZoomSupported()) {
+          subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_NOT_SUPPORT));
+          return;
         }
-        return Observable.create(new ObservableOnSubscribe<RxCamera>() {
-            @Override
-            public void subscribe(final ObservableEmitter<RxCamera> subscriber) throws Exception {
-                Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
-                if (parameters.getMaxNumFocusAreas() < focusAreaList.size()) {
-                    subscriber.onError(new SettingAreaFocusError(SettingAreaFocusError.Reason.NOT_SUPPORT));
-                } else {
-                    if (parameters.getFocusMode() != Camera.Parameters.FOCUS_MODE_AUTO) {
-                        List<String> focusModes = parameters.getSupportedFocusModes();
-                        if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
-                            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-                        }
-                    }
-                    parameters.setFocusAreas(focusAreaList);
-                    rxCamera.getNativeCamera().setParameters(parameters);
-                    rxCamera.getNativeCamera().autoFocus(new Camera.AutoFocusCallback() {
-                        @Override
-                        public void onAutoFocus(boolean success, Camera camera) {
-                            if (success) {
-                                subscriber.onNext(rxCamera);
-                            } else {
-                                subscriber.onError(new SettingAreaFocusError(SettingAreaFocusError.Reason.SET_AREA_FOCUS_FAILED));
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    }
-
-    public Observable<RxCamera> areaMeterAction(final List<Camera.Area> meterAreaList) {
-        if (meterAreaList == null || meterAreaList.size() == 0) {
-            return null;
+        int maxZoomLevel = parameters.getMaxZoom();
+        if (level < 0 || level > maxZoomLevel) {
+          subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_RANGE_ERROR));
+          return;
         }
-        return Observable.create(new ObservableOnSubscribe<RxCamera>() {
-            @Override
-            public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
-                Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
-                if (parameters.getMaxNumMeteringAreas() < meterAreaList.size()) {
-                    subscriber.onError(new SettingMeterAreaError(SettingMeterAreaError.Reason.NOT_SUPPORT));
-                } else {
-                    parameters.setFocusAreas(meterAreaList);
-                    rxCamera.getNativeCamera().setParameters(parameters);
-                    subscriber.onNext(rxCamera);
-                }
-            }
-        });
+        parameters.setZoom(level);
+        rxCamera.getNativeCamera().setParameters(parameters);
+        subscriber.onNext(rxCamera);
+      }
+    });
+  }
+
+  /**
+   * smooth zoom the camera, which will gradually change the preview content
+   *
+   * @param level
+   * @return
+   */
+  public Observable<RxCamera> smoothZoom(final int level) {
+    return Observable.create(new ObservableOnSubscribe<RxCamera>() {
+      @Override
+      public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
+        Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
+        if (!parameters.isZoomSupported() || !parameters.isSmoothZoomSupported()) {
+          subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_NOT_SUPPORT));
+          return;
+        }
+        int maxZoomLevel = parameters.getMaxZoom();
+        if (level < 0 || level > maxZoomLevel) {
+          subscriber.onError(new ZoomFailedException(ZoomFailedException.Reason.ZOOM_RANGE_ERROR));
+          return;
+        }
+        rxCamera.getNativeCamera().startSmoothZoom(level);
+        subscriber.onNext(rxCamera);
+      }
+    });
+  }
+
+  public Observable<RxCamera> flashAction(final boolean isOn) {
+    return Observable.create(new ObservableOnSubscribe<RxCamera>() {
+      @Override
+      public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
+        Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
+        if (parameters.getSupportedFlashModes() == null || parameters.getSupportedFlashModes().size() <= 0) {
+          subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
+          return;
+        }
+        if (isOn) {
+          if (parameters.getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_AUTO)) {
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+            rxCamera.getNativeCamera().setParameters(parameters);
+            subscriber.onNext(rxCamera);
+            return;
+          } else if (parameters.getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_ON)) {
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+            rxCamera.getNativeCamera().setParameters(parameters);
+            subscriber.onNext(rxCamera);
+            return;
+          } else if (parameters.getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_TORCH)) {
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            rxCamera.getNativeCamera().setParameters(parameters);
+            subscriber.onNext(rxCamera);
+            return;
+          } else {
+            subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
+          }
+        } else {
+          if (parameters.getSupportedFlashModes().contains(Camera.Parameters.FLASH_MODE_OFF)) {
+            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            rxCamera.getNativeCamera().setParameters(parameters);
+            subscriber.onNext(rxCamera);
+          } else {
+            subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
+          }
+        }
+      }
+    });
+  }
+
+  public Observable<RxCamera> flashAction(final String mode) {
+    return Observable.create(new ObservableOnSubscribe<RxCamera>() {
+      @Override
+      public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
+        Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
+        if (parameters.getSupportedFlashModes() == null || parameters.getSupportedFlashModes().size() <= 0) {
+          subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
+          return;
+        }
+
+        if (parameters.getSupportedFlashModes().contains(mode)) {
+          parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
+          rxCamera.getNativeCamera().setParameters(parameters);
+          subscriber.onNext(rxCamera);
+          return;
+        } else {
+          subscriber.onError(new SettingFlashException(SettingFlashException.Reason.NOT_SUPPORT));
+        }
+      }
+    });
+  }
+
+  public Observable<RxCamera> areaFocusAction(final List<Camera.Area> focusAreaList) {
+    if (focusAreaList == null || focusAreaList.size() == 0) {
+      return null;
     }
+    return Observable.create(new ObservableOnSubscribe<RxCamera>() {
+      @Override
+      public void subscribe(final ObservableEmitter<RxCamera> subscriber) throws Exception {
+        Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
+        if (parameters.getMaxNumFocusAreas() < focusAreaList.size()) {
+          subscriber.onError(new SettingAreaFocusError(SettingAreaFocusError.Reason.NOT_SUPPORT));
+        } else {
+          if (parameters.getFocusMode() != Camera.Parameters.FOCUS_MODE_AUTO) {
+            List<String> focusModes = parameters.getSupportedFocusModes();
+            if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+              parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            }
+          }
+          parameters.setFocusAreas(focusAreaList);
+          rxCamera.getNativeCamera().setParameters(parameters);
+          rxCamera.getNativeCamera().autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean success, Camera camera) {
+              if (success) {
+                subscriber.onNext(rxCamera);
+              } else {
+                subscriber.onError(new SettingAreaFocusError(SettingAreaFocusError.Reason.SET_AREA_FOCUS_FAILED));
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+
+  public Observable<RxCamera> areaMeterAction(final List<Camera.Area> meterAreaList) {
+    if (meterAreaList == null || meterAreaList.size() == 0) {
+      return null;
+    }
+    return Observable.create(new ObservableOnSubscribe<RxCamera>() {
+      @Override
+      public void subscribe(ObservableEmitter<RxCamera> subscriber) throws Exception {
+        Camera.Parameters parameters = rxCamera.getNativeCamera().getParameters();
+        if (parameters.getMaxNumMeteringAreas() < meterAreaList.size()) {
+          subscriber.onError(new SettingMeterAreaError(SettingMeterAreaError.Reason.NOT_SUPPORT));
+        } else {
+          parameters.setFocusAreas(meterAreaList);
+          rxCamera.getNativeCamera().setParameters(parameters);
+          subscriber.onNext(rxCamera);
+        }
+      }
+    });
+  }
 }
